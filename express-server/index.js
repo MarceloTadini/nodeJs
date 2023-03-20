5f989f2484291a82a6c21556a880fb458611599b
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-03-20 17:17:02 -0300

    adding ejs

--- express-server/index.js
+++ express-server/index.js
@@ -2,20 +2,23 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+//definindo o template engine
+app.set('view engine', 'ejs') //quando uso template engine, não preciso definir os arquivos estáticos
+
 //MVC -MODEL VIEW CONTROLLER
 
-//Definindo os arquivos estáticos
-const staticFolder = path.join(__dirname, 'views') //caminho da pasta views
-const expressStatic = express.static(staticFolder) // passamos a pasta de arquivos estáticos
-app.use(expressStatic) 
+// //Definindo os arquivos estáticos
+// const staticFolder = path.join(__dirname, 'views') //caminho da pasta views
+// const expressStatic = express.static(staticFolder) // passamos a pasta de arquivos estáticos
+// app.use(expressStatic) 
 
-//Definindo os arquivos estáticos
+//Definindo os arquivos públicos
 const publicFolder = path.join(__dirname, 'public') //caminho da pasta views
 const expressPublic = express.static(publicFolder) // passamos a pasta de arquivos estáticos
 app.use(expressPublic) 
 
 app.get('/', (request, response) => {
-    response.render('views/index')
+    response.render('index')
 })
 
 app.get('/about', (request, response) => {
