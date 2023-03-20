fbdf67378e7f72e3cc46ab563dcb2c959461a274
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-03-20 18:31:22 -0300

    using for and if

--- express-server/index.js
+++ express-server/index.js
@@ -17,12 +17,53 @@ const publicFolder = path.join(__dirname, 'public') //caminho da pasta views
 const expressPublic = express.static(publicFolder) // passamos a pasta de arquivos estáticos
 app.use(expressPublic) 
 
+
+//rotas
 app.get('/', (request, response) => {
-    response.render('index')
+    response.render('index', {
+        title: 'Digital Tech - Home'
+    })
 })
 
-app.get('/about', (request, response) => {
-    response.send("About")
+app.get('/quemsomos', (request, response) => {
+    response.send("quemsomos")
+})
+
+app.get('/produtos', (request, response) => {
+    response.send("produtos")
+
+})
+
+app.get('/servicos', (request, response) => {
+    response.send("servicos")
+})
+
+app.get('/contato', (request, response) => {
+    response.send("contato")
+})
+
+
+app.get('/posts', (request, response) => {
+    response.render("posts", {
+        title: 'Digital Tech - Posts',
+        posts: [
+            {
+                title: 'Novidade no mundo tech!', 
+                text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sit impedit vero quos deserunt eius asperiores totam, corporis voluptas culpa eos illo quisquam alias. Recusandae totam optio odio numquam sapiente.',
+                stars: 3
+            },
+            {
+                title: 'Novas vagas aberta!', 
+                text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sit impedit vero quos deserunt eius asperiores totam, corporis voluptas culpa eos illo quisquam alias. Recusandae totam optio odio numquam sapiente.',
+            
+            },
+            {
+                title: 'Acidente em fábrica de celular!', 
+                text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sit impedit vero quos deserunt eius asperiores totam, corporis voluptas culpa eos illo quisquam alias. Recusandae totam optio odio numquam sapiente.',
+                stars: 5
+            },
+        ]
+    })
 })
 
 //404 error (not found)
