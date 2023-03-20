0afb5c67ecf55f65f25f5ec3c5b004aab530739a
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-03-20 11:08:32 -0300

    static and public files with node

--- express-server/index.js
+++ express-server/index.js
@@ -1,15 +1,28 @@
-const { request, response } = require('express')
 const express = require('express')
+const path = require('path')
 const app = express()
 
+//MVC -MODEL VIEW CONTROLLER
+
+//Definindo os arquivos estáticos
+const staticFolder = path.join(__dirname, 'views') //caminho da pasta views
+const expressStatic = express.static(staticFolder) // passamos a pasta de arquivos estáticos
+app.use(expressStatic) 
+
+//Definindo os arquivos estáticos
+const publicFolder = path.join(__dirname, 'public') //caminho da pasta views
+const expressPublic = express.static(publicFolder) // passamos a pasta de arquivos estáticos
+app.use(expressPublic) 
+
 app.get('/', (request, response) => {
-    response.send("Hello World!")
+    response.render('views/index')
 })
 
 app.get('/about', (request, response) => {
     response.send("About")
 })
 
+//404 error (not found)
 app.use((request, response) => {
     response.send('Page not found!')
 })
---
+++ express-server/public/js/main.js
@@ -0,0 +1,1 @@
+console.log('ok,it is working')
